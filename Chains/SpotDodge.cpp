13c36be2ed1cfcd595741b2dd058220d402e7455
AltF4 <altf4@phx2600.org>
2016-01-30 19:23:30 -0700

    Support parrying charged smashes  - Also cleaned up parrying a bit

--- Chains/SpotDodge.cpp
+++ Chains/SpotDodge.cpp
@@ -6,6 +6,7 @@ void SpotDodge::PressButtons()
     //If we're medium range or more, and reverse-facing, don't bother doing anything.
     float distance = std::abs(m_state->m_memory->player_one_x - m_state->m_memory->player_two_x);
     if(m_isReverseFacing && (distance > 18))
+<<<<<<< HEAD
     {
         m_controller->emptyInput();
         return;
@@ -14,6 +15,8 @@ void SpotDodge::PressButtons()
     if(m_state->m_memory->player_two_action == SHIELD ||
         m_state->m_memory->player_two_action == SHIELD_START ||
         m_state->m_memory->player_two_action == SHIELD_REFLECT)
+=======
+>>>>>>> 7eada1c... Support parrying charged smashes
     {
         m_hasShielded = true;
     }
@@ -25,11 +28,36 @@ void SpotDodge::PressButtons()
         return;
     }
 
+<<<<<<< HEAD
     if(!m_hasSpotDodged && m_state->m_memory->player_two_action == SHIELD_REFLECT)
     {
         m_controller->tiltAnalog(Controller::BUTTON_MAIN, .5, 0);
         m_hasSpotDodged = true;
         return;
+=======
+    switch(m_state->m_memory->player_one_action_frame)
+    {
+        case 1:
+        {
+            //Spot dodge
+            m_controller->tiltAnalog(Controller::BUTTON_MAIN, .5, .5);
+            m_controller->pressButton(Controller::BUTTON_L);
+            break;
+        }
+        case 2:
+        {
+            //down
+            m_controller->tiltAnalog(Controller::BUTTON_MAIN, .5, 0);
+            break;
+        }
+        case 3:
+        {
+            //let go
+            m_controller->tiltAnalog(Controller::BUTTON_MAIN, .5, .5);
+            m_controller->releaseButton(Controller::BUTTON_L);
+            break;
+        }
+>>>>>>> 7eada1c... Support parrying charged smashes
     }
 
     m_controller->emptyInput();
@@ -47,9 +75,12 @@ bool SpotDodge::IsInterruptible()
 
 SpotDodge::SpotDodge()
 {
+<<<<<<< HEAD
     m_startingFrame = m_state->m_memory->frame;
     m_hasShielded = false;
     m_hasSpotDodged = false;
+=======
+>>>>>>> 7eada1c... Support parrying charged smashes
     bool player_one_is_to_the_left = (m_state->m_memory->player_one_x - m_state->m_memory->player_two_x > 0);
     if(m_state->m_memory->player_one_facing != player_one_is_to_the_left)
     {
